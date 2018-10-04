a88102a9ddf695e82462e0c1700a1843f63dcbf6
Stabel,Jordy J <j.stabel@student.fontys.nl>
2018-10-04 13:07:06 +0200

    Latest working version

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour { 
+    public void PlayGame()
+    {
+        PlayerPrefs.SetInt("currentLevel", 0);
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        PlayerPrefs.SetInt("currentLevel", 0);
+        Application.Quit();
+    }
+}
